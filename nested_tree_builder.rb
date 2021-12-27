6d2fad07fa63797c30e617367681859ab260a071
Rafik Dali <rfkdali@gmail.com>
2021-12-27 18:31:34 +0100

    Better naming, fixed specs

--- nested_tree_builder.rb
+++ nested_tree_builder.rb
@@ -1,11 +1,67 @@
 class NestedTreeBuilder
-  attr_reader :flat_list
+  attr_reader :all_categories, :category_tree
 
-  def initialize(flat_list)
-    @flat_list = flat_list
+  def initialize(all_categories)
+    @all_categories = all_categories
+    @category_tree  = []
   end
 
   def build_tree
-    flat_list.sort.map {|item| { title: item } }
+    all_categories.sort.each do |item|
+      current_category(item) << new_category(item)
+    end
+    category_tree
+  end
+
+  private
+
+  def new_category(item)
+    { title: item, children: []}
+  end
+
+  def category_level(item)
+    item.scan(/\d+./).size
+  end
+
+  def parent_category(item)
+    # return 1. if item is 1.1
+    item.scan(/\d+./).first(category_level(item)- 1).join.concat(' ')
+  end
+
+  def root?(item)
+    category_level(item) < 2
+  end
+
+  def current_category(item)
+    return category_tree if root?(item)
+
+    title = parent_category(item)[0..1].concat(' ')
+    parent = category_tree.find {|t| t[:title].scan(title)[0] }
+    parent = parent[:children].find {|c| c[:title].scan(/^#{parent_category(item)}/)[0]} || parent
+    parent[:children]
   end
 end
+
+list = [
+        "1. Shoes",
+        "2.2. Jewels",
+        "1.2. Boots",
+        "2.2.2. Watches. Bracelets",
+        "2. Accessories",
+        "2.3. Gloves",
+        "2.2.1. Rings",
+        "2.1. Hats",
+        "1.1. Heels",
+        "1.2.3 Groceries"
+      ]
+
+p 'Input:'
+p '------'
+p list
+
+nested_tree = NestedTreeBuilder.new(list)
+
+p '------'
+p 'Output:'
+p '------'
+pp nested_tree.build_tree
--- spec/nested_tree_builder_spec.rb
+++ spec/nested_tree_builder_spec.rb
@@ -1,14 +1,12 @@
 require_relative '../nested_tree_builder'
-
 require 'rspec'
-require 'pry'
 
 describe NestedTreeBuilder do
   context 'Without sub levels' do
     let(:ordered_list) { ["1. Shoes","2. Accessories"] }
     let(:unordered_list) { ["2. Accessories", "1. Shoes"] }
     let(:expected) do
-      [{"title": "1. Shoes"}, {"title": "2. Accessories"}]
+      [{"title": "1. Shoes", "children": []}, {"title": "2. Accessories", "children": []}]
     end
     context 'From ordered list' do
       it 'builds tree with titles' do
@@ -24,5 +22,66 @@ describe NestedTreeBuilder do
   end
 
   context 'With sub levels' do
+    let(:list) do
+      [ "1. Shoes", "1.1. Heels", "2. Accessories", "2.1. Hats" ]
+    end
+    let(:expected) do
+      [
+        { "title": "1. Shoes",
+          "children": [{"title": "1.1. Heels", "children": []}]
+        },
+        { "title": "2. Accessories",
+          "children": [{"title": "2.1. Hats", "children": []}]
+        }
+      ]
+    end
+
+    it 'builds tree with titles and children' do
+      expect(described_class.new(list).build_tree).to eq(expected)
+    end
+  end
+
+  context 'With deep sub levels' do
+    let(:list) do
+      [
+        "1. Shoes",
+        "2.2. Jewels",
+        "1.2. Boots",
+        "2.2.2. Watches. Bracelets",
+        "2. Accessories",
+        "2.3. Gloves",
+        "2.2.1. Rings",
+        "2.1. Hats",
+        "1.1. Heels",
+        "1.2.3 Groceries"
+      ]
+    end
+    let(:expected) do
+      [
+        {
+          "title": "1. Shoes", "children": [
+            {"title": "1.1. Heels", "children": []},
+            {"title": "1.2. Boots", "children": [
+              {"title": "1.2.3 Groceries", "children": []}
+            ]}
+          ]
+        },
+        {
+          "title": "2. Accessories", "children": [
+            {"title": "2.1. Hats", "children": []},
+            {"title": "2.2. Jewels", "children": [
+                {"title": "2.2.1. Rings", "children": []},
+                {"title": "2.2.2. Watches. Bracelets", "children": []}
+              ]
+            },
+            {"title": "2.3. Gloves", "children": []}
+          ]
+        }
+      ]
+    end
+
+    it 'builds tree with titles and children' do
+      expect(described_class.new(list).build_tree).to eq(expected)
+    end
   end
 end
